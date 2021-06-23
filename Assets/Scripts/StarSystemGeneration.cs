99c12be535cc62c98617762e66f7422f4ba82573
Salsabanana <Axelchanoine@gmail.com>
2021-06-23 16:27:41 +0200

    parsing star system content jsons

--- Assets/Scripts/StarSystemGeneration.cs
+++ Assets/Scripts/StarSystemGeneration.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class StarSystemGeneration : MonoBehaviour
 {
+    public GameObject SSContentPrefab;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,58 @@ public class StarSystemGeneration : MonoBehaviour
 
     public void LoadSystem(GameObject starSystem)
     {
-        print(starSystem);
+        
+        GameObject SSContent = (GameObject)Instantiate(SSContentPrefab, starSystem.transform.position, Quaternion.identity);
+        SSContent.transform.parent = starSystem.transform;
+        SystemsInfosScript starSystemInfos = starSystem.GetComponent<SystemsInfosScript>();
+        SSData jsonSSData = JsonUtility.FromJson<SSData>(starSystemInfos.json);
+        SSContent.name = jsonSSData.data.rowcount;
+
+        foreach (SystemContent systemContent in jsonSSData.data.resultset)
+        {
+            SSContent.name = systemContent.name;
+
+            foreach(CelestialObject celestialObject in systemContent.celestial_objects)
+            {
+                print(celestialObject.type);
+            }
+        }
+        
     }
+
+
+
+    [System.Serializable]
+    public class SSData
+    {
+        public Data data;
+        
+    }
+
+    [System.Serializable]
+    public class Data
+    {
+        public string rowcount;
+        public SystemContent[] resultset;
+    }
+
+
+
+    [System.Serializable]
+    public class SystemContent
+    {
+        public string name;
+        public CelestialObject[] celestial_objects;
+
+    }
+
+    
+    
+    [System.Serializable]
+    public class CelestialObject
+    {
+        public string type;
+
+    }
+
 }
