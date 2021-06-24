0db43f1c8d3e245054a9901b18703bf8844d5b75
Salsabanana <Axelchanoine@gmail.com>
2021-06-24 21:44:28 +0200

    planet and moon spawning

--- Assets/Scripts/StarSystemGeneration.cs
+++ Assets/Scripts/StarSystemGeneration.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Globalization;
 public class StarSystemGeneration : MonoBehaviour
 {
     public GameObject SSContentPrefab;
-
-
-
+    public GameObject starPrefab;
+    public GameObject bluePlanetPrefab;
+    public GameObject brownPlanetPrefab;
+    public GameObject greenPlanetPrefab;
+    public GameObject gazPlanetPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +26,92 @@ public class StarSystemGeneration : MonoBehaviour
     public void LoadSystem(GameObject starSystem)
     {
         
-        GameObject SSContent = (GameObject)Instantiate(SSContentPrefab, starSystem.transform.position, Quaternion.identity);
-        SSContent.transform.parent = starSystem.transform;
+        GameObject SSContentGO = Instantiate(SSContentPrefab, starSystem.transform.position, Quaternion.identity);
+        SSContentGO.transform.parent = starSystem.transform;
         SystemsInfosScript starSystemInfos = starSystem.GetComponent<SystemsInfosScript>();
         SSData jsonSSData = JsonUtility.FromJson<SSData>(starSystemInfos.json);
-        SSContent.name = jsonSSData.data.rowcount;
 
         foreach (SystemContent systemContent in jsonSSData.data.resultset)
         {
-            SSContent.name = systemContent.name;
+            SSContentGO.name = systemContent.name;
+            var planetList = new List<KeyValuePair<GameObject, int>>();
 
             foreach(CelestialObject celestialObject in systemContent.celestial_objects)
             {
-                print(celestialObject.type);
+                GameObject celestialGO;
+                if(celestialObject.type == "STAR")
+                {
+                    celestialGO = Instantiate(starPrefab, starSystem.transform.position, Quaternion.identity);
+                    celestialGO.name = celestialObject.designation;
+                    celestialGO.transform.parent = SSContentGO.transform;
+                    celestialGO.transform.localScale /= 3;
+                }
+                if(celestialObject.type == "PLANET")
+                {
+                    
+                    switch(celestialObject.appearance)
+                    {
+                        case "PLANET_BLUE":
+                            celestialGO = Instantiate(bluePlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        case "PLANET_BROWN":
+                            celestialGO = Instantiate(brownPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        case "PLANET_GREEN":
+                            celestialGO = Instantiate(greenPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        case "PLANET_GAS":
+                            celestialGO = Instantiate(gazPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        default:
+                            celestialGO = Instantiate(brownPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                    }
+                    celestialGO.name = celestialObject.name;
+
+                    float longitude = celestialObject.longitude * Mathf.Deg2Rad;
+                    float distance = celestialObject.distance * 5;
+                
+                    celestialGO.transform.parent = SSContentGO.transform;
+                    celestialGO.transform.localPosition = new Vector3(distance * Mathf.Cos(longitude), 0, distance * Mathf.Sin(longitude));
+                    celestialGO.transform.localScale /= 3;
+
+                    planetList.Add(new KeyValuePair<GameObject, int>(celestialGO, celestialObject.id));
+                    
+                }
+                if(celestialObject.type == "SATELLITE")
+                {
+                    switch(celestialObject.appearance)
+                    {
+                        case "PLANET_BLUE":
+                            celestialGO = Instantiate(bluePlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        case "PLANET_BROWN":
+                            celestialGO = Instantiate(brownPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        case "PLANET_GREEN":
+                            celestialGO = Instantiate(greenPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                        default:
+                            celestialGO = Instantiate(brownPlanetPrefab, starSystem.transform.position, Quaternion.identity);
+                            break;
+                    }
+                    
+                    foreach(KeyValuePair<GameObject, int> planet in planetList)
+                    {
+                        if(planet.Value == celestialObject.parent_id) celestialGO.transform.parent = planet.Key.transform;
+                    }
+                    celestialGO.name = celestialObject.name;
+
+                    float longitude = celestialObject.longitude * Mathf.Deg2Rad;
+                    float distance = celestialObject.distance * 1000;
+
+                    celestialGO.transform.localPosition = new Vector3(distance * Mathf.Cos(longitude), 0, distance * Mathf.Sin(longitude));
+                    celestialGO.transform.localScale /= 6;
+                }
+            
             }
         }
-        
     }
 
 
@@ -54,7 +126,6 @@ public class StarSystemGeneration : MonoBehaviour
     [System.Serializable]
     public class Data
     {
-        public string rowcount;
         public SystemContent[] resultset;
     }
 
@@ -74,7 +145,13 @@ public class StarSystemGeneration : MonoBehaviour
     public class CelestialObject
     {
         public string type;
-
+        public string designation;
+        public string name;
+        public string appearance;
+        public float longitude;
+        public float distance;
+        public int id;
+        public int parent_id;
     }
 
 }
